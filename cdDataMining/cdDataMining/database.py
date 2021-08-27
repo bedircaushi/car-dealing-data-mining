9dca923b0f1298003b31d26ef000328b771b9592
bedircaushi <bedircaushi1@gmail.com>
2021-08-27 11:34:13 +0200

    push

--- cdDataMining/cdDataMining/database.py
+++ cdDataMining/cdDataMining/database.py
@@ -1,25 +1,23 @@
-import pymysql.cursors
+from pymongo import MongoClient
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='1234',
-                             db='CarDealer',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+client=MongoClient('mongodb+srv://cardealer:2901999470038@cluster0.gst4z.mongodb.net/test')
 
-def insertCar(vehicleID, make, model, description, fuel, image, price, power_, mileage, date_, username):
-    
+db=client["CarDealer-api"]
+collection=db.cars
 
-        with connection.cursor() as cursor:
-
-            sql = "INSERT INTO car (vehicleID, make, model, description_, fuel, image, price, power_, mileage, date_, username) VALUES (%s, %s,%s,%s,%s,%s, %s,%s,%s,%s,%s)"
-            val = (vehicleID, make, model, description, fuel, image, price, power_, mileage, date_, username)
-
-            cursor.execute(sql, val)
-            connection.commit()
-
-    # finally:
-    #     connection.close()
-
-# insertCar("aasjdnajksndkjsaf23234dsaas", 'audi', 'a4', "Audi a4 2.0 tdi facelift", "petrol", "https://prod.pictures.autoscout24.net/listing-images/f31af767-bea3-4d8e-848d-27359d150d87_27ee75d3-a430-4d2a-98b2-faafcda7b52e.jpg/640x480.jpg",
-#             12000,"120",40000,'2018-02-01 00:00:00','bedircaushi')
+def insertCar(make,year,capacity,price,carmodel,fuel,registration,mileage,fuelConsumption,emissionClass,description,features,gearbox,owner):
+     collection.insert_one({"make":make,
+            "year":year,
+            "capacity":capacity,
+            "price":price,
+            "carmodel":carmodel,
+            "fuel":fuel,
+            "registration":registration,
+            "mileage":mileage,
+            "fuelConsumption":fuelConsumption,
+            "emissionClass":emissionClass,
+            "description":description,
+            "features":features,
+            "gearbox":gearbox,
+            "owner":owner
+            })
--- cdDataMining/cdDataMining/spiders/MobiledeSpider.py
+++ cdDataMining/cdDataMining/spiders/MobiledeSpider.py
@@ -7,6 +7,7 @@ import hashlib
 from hashlib import sha224
 from datetime import datetime
 from ..database import *
+from bson.objectid import ObjectId
 
 
 class MobiledeSpider(scrapy.Spider):
@@ -18,59 +19,50 @@ class MobiledeSpider(scrapy.Spider):
         self.pushData=[]
 
     def start_requests(self):
-        urls = ["https://www.autoscout24.com/lst/renault?sort=price&desc=0&custtype=D&doorfrom=4&doorto=5&ustate=N%2CU&size=20&page=20&powerfrom=120&powertype=kw&fregfrom=2013&atype=C&ac=0&"]
+        urls = ["https://www.autoscout24.com/lst/audi?sort=price&desc=0&custtype=D&doorfrom=4&doorto=5&ustate=N%2CU&size=20&page=20&powerfrom=120&powertype=kw&fregfrom=2013&atype=C&ac=0&"]
         for url in urls:
             request = scrapy.Request(url, self.parse)
             yield request
     
     def parse(self, response):
-        cars_section = response.xpath("//div[2]/div[3]/div[2]/div[2]/div/div/div")
+        cars_section = response.xpath("/html/body/div[1]/div[9]/div[4]/div[2]/div[3]/div[2]/div[2]/div/div/div/div/div/div[1]")
         i=1
         for car in cars_section:
+            print("here")
         
             ilink = car.xpath("//div[@class='cl-list-element cl-list-element-gap'][{}]//div[@class='cldt-summary-titles']/a/@href".format(i)).extract_first()
             i+=1
+            print(ilink)
             if ilink is not None:
-                print(ilink)
                 request = scrapy.Request('https://www.autoscout24.com' + ilink, callback=self.parseCarDetail, dont_filter=True)
                 yield request
             
     def parseCarDetail(self, response):
-        try:
-            price = response.selector.xpath("/html/body/div[1]/main/div[3]/div[3]/div[1]/div[1]/h2/text()").extract_first()
-            price = price[2:-3]
-            print(price)
-            car = response.selector.xpath("/html/body/div[1]/main/div[2]/div/div[1]/h1/span/text()").extract_first().strip()
-            desc=response.selector.xpath("/html/body/div[1]/main/div[2]/div/div[1]/h1/span[2]/text()").extract_first() 
-            make = car.split(" ")[0]
-            model = car.split(" ")[1]
-            description = car + ' ' + desc
-            power=response.selector.xpath("/html/body/div[1]/main/div[3]/div[3]/div[2]/div[1]/div[3]/span[1]/text()").extract_first().split(" ")[0]
-            transmission = response.selector.xpath("/html/body/div[1]/main/div[3]/div[3]/div[2]/div[1]/div[3]/span[3]/text()").extract_first().split(" ")[0][:-1]
-            fuel = response.selector.xpath("/html/body/div[1]/main/div[3]/div[3]/div[2]/div[1]/div[3]/span[3]/text()").extract_first().split(" ")[1]
-            date=response.selector.xpath("/html/body/div[1]/main/div[3]/div[3]/div[2]/div[1]/div[2]/span[1]/text()").extract_first()
-            date = datetime.strptime(date, "%m/%Y")
-            mileage=response.selector.xpath("/html/body/div[1]/main/div[3]/div[3]/div[2]/div[1]/div[1]/span/text()").extract_first().split(" ")[0]
-            image=response.selector.xpath("//div//div[@class='as24-pictures__content']//img/@src").extract_first()
-            hash = sha224(make.encode() + image.encode()).hexdigest()
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            price = int(price.replace(',',''))
-            mileage=int(mileage.replace(',',''))
-        except:
-            pass
+        make = response.selector.xpath("/html/body/div[1]/main/div[3]/div/div/div[7]/div[2]/div[1]/div[2]/dl/dd[1]/text()").extract_first().strip()
+        year=int(response.selector.xpath("/html/body/div[1]/main/div[2]/div[3]/div[2]/div[1]/div[2]/span[1]/text()").extract_first().split("/")[1])
+        capacity=response.selector.xpath("/html/body/div[1]/main/div[2]/div[3]/div[2]/div[1]/div[3]/span[1]/text()").extract_first().split(" ")[0]
+        price = response.selector.xpath("/html/body/div[1]/main/div[2]/div[3]/div[1]/div[1]/h2/text()").extract_first()
+        price = int(price[2:-3].strip().replace(',',''))
+        model = response.selector.xpath("/html/body/div[1]/main/div[3]/div/div/div[7]/div[2]/div[1]/div[2]/dl/dd[2]/a/text()").extract_first().strip()
+        fuel = response.selector.xpath("/html/body/div[1]/main/div[2]/div[3]/div[2]/div[1]/div[3]/span[3]/text()").extract_first().split(" ")[1]
+        mileage=int(response.selector.xpath("/html/body/div[1]/main/div[2]/div[3]/div[2]/div[1]/div[1]/span/text()").extract_first().split(" ")[0].replace(',',''))
+        desc=response.selector.xpath("/html/body/div[1]/main/div[1]/div/div[1]/h1/span[2]/text()").extract_first().strip()
+        gearbox = response.selector.xpath("/html/body/div[1]/main/div[2]/div[3]/div[2]/div[1]/div[3]/span[3]/text()").extract_first().split(" ")[0][:-1]
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+ 
     
-        # print(description)
-        # print(model)
-        # print(make)
-        # print(price)
-        # print(power)
-        # print(transmission)
-        # print(fuel)
-        # print(date)
-        # print(mileage)
-        # print(type(date))
-        # print(type(price))
-        # print(type(mileage))
-        # print(image)
-        insertCar(hash,make,model,description,fuel,image,price,power,mileage,date,'agniramadani')
+        print(desc)
+        print(model)
+        print(make)
+        print(price)
+        print(fuel)
+        print(gearbox)
+        print(fuel)
+        print(year)
+        print(mileage)
+        print(capacity)
+        print(type(year))
+        print(type(price))
+        print(type(mileage))
+        insertCar(make,year,capacity,price,model,fuel,"macedonian",mileage,{},"euro4",desc,[],gearbox,ObjectId("5f9dd3ba42915114689913e6"))
   
